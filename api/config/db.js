c49e05aaad1002b2b1687051895f435472d2a479
Hamna Altaf <hamnaaltaf23@gmail.com>
2026-08-24 15:58:09 +0530

    Fix db.js connection for Vercel

--- api/config/db.js
+++ api/config/db.js
@@ -12,3 +12,24 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cached.conn) return cached.conn;
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      bufferCommands: false,
+      maxPoolSize: 10,
+    }).then(mongoose => mongoose);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
+
+module.exports = connectDB;
